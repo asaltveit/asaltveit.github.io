aaf02a02f6caab0d03370aa144584b8182fa78f5
Anna <annasaltveit@gmail.com>
2025-04-07 14:57:40 -0700

    move test workflow into publish

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,3 +32,23 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+/*
+publish.yml
+
+a separate test.yml file and 
+
+on: 
+  workflow_run:
+      workflows: [test]
+      types: [completed]
+
+and
+
+if: ${{ github.event.workflow_run.conclusion == 'success' }}
+
+OR
+
+Does the checkout+set up need to be done twice?
+*/
