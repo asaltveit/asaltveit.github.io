bad2f01e35502187ea789659c06ee321b1ec187a
Anna <annasaltveit@gmail.com>
2025-10-30 06:45:11 -0700

    Fix project tests

--- __tests__/project.test.jsx
+++ __tests__/project.test.jsx
@@ -2,7 +2,6 @@ import '@testing-library/jest-dom'
 import { render, screen, within } from '@testing-library/react'
 import './mocks/matchMediaFalse.mock'
 import Project from '@/components/Project'
-import mapScreenshotSrc from "../public/map-screenshot-032725.png"
 
 const matchMediaMock = jest.spyOn(window, 'matchMedia');
 
@@ -11,7 +10,7 @@ beforeEach(() => {
 });
  
 describe('Project', () => {
-    it('renders without image', () => {
+    it('renders', () => {
         render(<Project title="Test" dates="1-1" items={["bullet1", "bullet2"]} />)
         const title = screen.getByText('Test')
         expect(title).toBeInTheDocument()
@@ -33,70 +32,5 @@ describe('Project', () => {
         expect(item2).toBeInTheDocument()
 
         expect(screen.getByRole('link', {name: 'link to Test'})).toHaveAttribute('href', '#')
-
-        const imageElement = document.querySelector('img');
-        expect(imageElement).not.toBeInTheDocument();
-    })
-    it('renders with image and not isEven', () => {
-        render(<Project title="Test" dates="1-1" items={["bullet1", "bullet2"]} image={mapScreenshotSrc} />)
-        const title = screen.getByText('Test')
-        expect(title).toBeInTheDocument()
-        
-        const dates = screen.getByText('1-1')
-        expect(dates).toBeInTheDocument()
-
-        const list = screen.getByRole("list", {
-            name: "",
-        })
-        const { getAllByRole } = within(list)
-        const items = getAllByRole("listitem")
-        expect(items.length).toBe(2)
-        
-        const item1 = screen.getByText('bullet1')
-        expect(item1).toBeInTheDocument()
-        
-        const item2 = screen.getByText('bullet2')
-        expect(item2).toBeInTheDocument()
-
-        expect(screen.getByRole('link', {name: 'link to Test'})).toHaveAttribute('href', '#')
-
-        const imageElement = document.querySelector('img');
-        expect(imageElement.alt).toBe('image screenshot');
-
-        expect(screen.getByRole('link', {name: 'image link to Test'})).toHaveAttribute('href', '#')
-    })
-    it('renders with image and isEven', () => {
-        render(<Project title="Test" dates="1-1" items={["bullet1", "bullet2"]} image={mapScreenshotSrc} isEven />)
-        const title = screen.getByText('Test')
-        expect(title).toBeInTheDocument()
-        
-        const dates = screen.getByText('1-1')
-        expect(dates).toBeInTheDocument()
-
-        const list = screen.getByRole("list", {
-            name: "",
-        })
-        const { getAllByRole } = within(list)
-        const items = getAllByRole("listitem")
-        expect(items.length).toBe(2)
-        
-        const item1 = screen.getByText('bullet1')
-        expect(item1).toBeInTheDocument()
-        
-        const item2 = screen.getByText('bullet2')
-        expect(item2).toBeInTheDocument()
-
-        expect(screen.getByRole('link', {name: 'link to Test'})).toHaveAttribute('href', '#')
-
-        const imageElement = document.querySelector('img');
-        expect(imageElement.alt).toBe('image image');
-
-        expect(screen.getByRole('link', {name: 'image link to Test'})).toHaveAttribute('href', '#')    })
-
-    it('renders for mobile', () => {
-        matchMediaMock.mockReturnValueOnce({ matches: true });
-        const imageElement = document.querySelector('img');
-        expect(imageElement).not.toBeInTheDocument();
-        // TODO
     })
 })
